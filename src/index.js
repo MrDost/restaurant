c8ddb1e6a98afd6865260a42707428941b5f89e6
hopeflufful <hopeflufful@gmail.com>
2024-01-13 17:45:01 +0300

    trblsht

--- src/index.js
+++ src/index.js
@@ -4,21 +4,17 @@ import renderDish from './kitchen.js';
 
 const ulElement = document.querySelector('ul');
 
-ulElement.addEventListener('touchend', (event) => {
+ulElement.addEventListener('click', (event) => {
     const targetLi = event.target.closest('li');
     if (targetLi) {
         const index = Array.from(ulElement.children).indexOf(targetLi);
         clearContent();
         if (index === 0) {
             renderDish('pizza');
-            alert('pizza');
         } else if (index === 1) {
             renderDish('burger');
-            alert('burger');
         } else if (index === 2) {
-            console.log(index);
             renderDish('sushi');
-            alert('sushi');
         }
     }
 });
